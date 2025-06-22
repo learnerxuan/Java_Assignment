bcc7252fd2daf5a4fb9d48194e98d2ec76dc4f2e
EeSern <168263601+EeSern@users.noreply.github.com>
2025-06-22 19:04:37 +0800

    updated receptionist class

--- src/main/java/com/atu/atc/model/Receptionist.java
+++ src/main/java/com/atu/atc/model/Receptionist.java
@@ -12,23 +12,6 @@ public class Receptionist extends User {
         return getId().equals(enteredId) && getPassword().equals(enteredPassword);
     }
 
-    // Register new student
-    public void registerStudent() {
-        
-    }
-
-    public void enrollStudentInSubject(String studentId, String classId){
-        // Placeholder only
-        System.out.println("Enrolling student " + studentId + " into class " + classId);
-    }
-
-    public void updateEnrollment(){}
-
-    public void acceptPayment() { /* ... */ }
-    // Add all methods from your assignment description for Receptionist
-
-    public void deleteStudent(){}
-
     // No need update profile override since it's the same
 
     public String toFileString(){
