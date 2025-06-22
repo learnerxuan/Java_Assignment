e822643691a2329988b27c3566d57b36179161a7
EeSern <168263601+EeSern@users.noreply.github.com>
2025-06-22 17:06:03 +0800

    Renewed UserRepository.java

--- src/main/java/com/atu/atc/data/UserRepository.java
+++ src/main/java/com/atu/atc/data/UserRepository.java
@@ -7,8 +7,59 @@ package com.atu.atc.data;
 import com.atu.atc.model.User;
 import com.atu.atc.util.FileUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Generic abstract class for user repositories
 // T must extend User
 public abstract class UserRepository<T extends User> {
+    protected List<T> users = new ArrayList<>();
 
+    // Load user from file into memory
+    public abstract void load();
+
+    // Save user to file from memory
+    public abstract void save();
+
+    // Get user by ID
+    public T getById(String id){
+        for (T user : users){
+            if (user.getId().equals(id)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    // Get all users
+    public List<T> getAll(){
+        return users;
+    }
+
+    // Add a new user and save
+    public void add(T user){
+        users.add(user);
+        save();
+    }
+
+    // Update existing user
+    public boolean update(T updatedUser){
+        for (int i =0; i < users.size(); i++){
+            if (users.get(i).getId().equals(updatedUser.getId())){
+                users.set(i, updatedUser);
+                save();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Delete a user by ID
+    public boolean delete(String id){
+        boolean removed = users.removeIf(u -> u.getId().equals(id));
+        if (removed){
+            save();
+        }
+        return removed;
+    }
 }
